a6e37b9fdbce65e2d4da12253c8d2ada1206fac0
Ilkham Jumatov <batir2jumatov@gmail.com>
2023-12-04 10:41:54 +0500

    Implemented compareTo method for idetifying a winner

--- src/main/java/com/ilkhamjumatov/entities/GameObject.java
+++ src/main/java/com/ilkhamjumatov/entities/GameObject.java
@@ -4,9 +4,21 @@ public abstract class GameObject {
     protected final String name;
     protected final int id;
 
+    private int[][] result = {
+            //rock  paper  scissors
+            {   0,    -1,      1}, // rock
+            {   1,     0,     -1},  // paper
+            {  -1,     1,      0},  // scissors
+    };
+
     public GameObject(String name, int id) {
         this.name = name;
         this.id = id;
+    }
+
+    public int compareTo(GameObject obj) {
+
+        return result[this.id][obj.id];
     }
 
     public abstract String toString();
