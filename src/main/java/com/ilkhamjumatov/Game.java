431ad38c498991bcf8a97136778d4e4a1286a9cb
Ilkham Jumatov <batir2jumatov@gmail.com>
2023-12-04 09:30:02 +0500

    created Game class

---
+++ src/main/java/com/ilkhamjumatov/Game.java
@@ -0,0 +1,14 @@
+package com.ilkhamjumatov;
+
+public class Game {
+
+    public void run() {
+        startGameLoop();
+    }
+
+    public void startGameLoop(){
+
+        System.out.println("The game loop is working");
+
+    }
+}
--- src/main/java/com/ilkhamjumatov/Main.java
+++ src/main/java/com/ilkhamjumatov/Main.java
@@ -1,8 +1,12 @@
 package com.ilkhamjumatov;
 
+import com.ilkhamjumatov.entities.GameObject;
+import com.ilkhamjumatov.entities.Paper;
+import com.ilkhamjumatov.entities.Rock;
+import com.ilkhamjumatov.entities.Scissors;
+
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello, World!");
+        new Game().run();
     }
 }
